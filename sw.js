d2ee46334a698ae27969bf04960f06932f26a353
umamiulcinj <dinohaxhibrahimi@gmail.com>
2024-01-11 01:50:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const cacheVersion = 3.3;  // Increment this version number
+const cacheVersion = 3.4;  // Increment this version number
 const cacheName = `umami-v${cacheVersion}`;
 const filesToCache = [
   '/',
